7381e57be12915e31c343a1efe436224f1765794
VanesaGC <vanesa.garcia.c@gmail.com>
2018-12-31 09:31:11 +0000

    variables de entorno para desarrollo o producción

--- 03webpack/webpack.common.js
+++ 03webpack/webpack.common.js
@@ -1,69 +1,76 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    resolve: {
-        extensions: ['.js', '.ts', 'tsx']
-    },
-    entry: {
-        app: './src/tsx/index.tsx',
-        appStyles: [
-            './src/sass/mystyles.scss',
-        ],
-    },
-    output: {
-        filename: 'js/[name].js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                loader: 'awesome-typescript-loader',
-                options: {
-                    useBabel: true,
-                    "babelCore": "@babel/core", // needed for Babel v7
+module.exports = env => {
+
+    // Use env.<YOUR VARIABLE> here:
+    console.log('NODE_ENV: ', env.NODE_ENV); // 'local'
+    console.log('Production: ', env.production); // true
+
+    return {
+        resolve: {
+            extensions: ['.js', '.ts', 'tsx']
+        },
+        entry: {
+            app: './src/tsx/index.tsx',
+            appStyles: [
+                './src/sass/mystyles.scss',
+            ],
+        },
+        output: {
+            filename: 'js/[name].js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/,
+                    exclude: /node_modules/,
+                    loader: 'awesome-typescript-loader',
+                    options: {
+                        useBabel: true,
+                        "babelCore": "@babel/core", // needed for Babel v7
+                    },
                 },
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.(png|jpg)$/,
-                exclude: /node_modules/,
-                loader: 'url-loader',
-                options: {
-                    limit: 5000,
-                    name: './img/[name].[ext]',
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader",
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader"
-                ]
-            },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                },
+                {
+                    test: /\.(png|jpg)$/,
+                    exclude: /node_modules/,
+                    loader: 'url-loader',
+                    options: {
+                        limit: 5000,
+                        name: './img/[name].[ext]',
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "sass-loader",
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader"
+                    ]
+                },
+            ],
+        },
+        plugins: [
+            //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+            new HtmlWebpackPlugin({
+                filename: 'index.html', //Name of file in ./dist/
+                template: './src/index.html', //Name of template in ./src
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].css"
+            }),
         ],
-    },
-    plugins: [
-        //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
-        new HtmlWebpackPlugin({
-            filename: 'index.html', //Name of file in ./dist/
-            template: './src/index.html', //Name of template in ./src
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css"
-        }),
-    ],
+    };
 };
