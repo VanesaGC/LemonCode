8f6d59edce96664dbfe9058cd7da39424b26954d
VanesaGC <vanesa.garcia.c@gmail.com>
2019-01-26 10:08:44 +0000

    Ejercicios angular y vuejs

---
+++ 04_vuejs/00_start/src/components/memberTable/Table.vue
@@ -0,0 +1,58 @@
+<template>
+<v-app>
+  <div>
+    <h2>Member Page</h2>
+    <input :value="organization" @input="organization = $event.target.value"/>
+    <p>Search: {{organization}}</p>
+    <button @click="loadMembers">Load</button>
+    <table :class="$style.table">
+      <thead>
+        <member-head/>
+      </thead>
+      <tbody>
+        <template v-for="member in members">
+          <member-row :key="member.id" :member="member"/>
+        </template>
+      </tbody>
+    </table>
+  </div>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import MemberHead from "./Head.vue";
+import MemberRow from "./Row.vue";
+import { Member } from "../../model/member";
+import { getAllMembers } from "../../api/memberAPI";
+
+export default Vue.extend({
+  name: "MemberTable",
+  components: { MemberHead, MemberRow },
+  data: () => ({
+    organization: "lemoncode",
+    members: [] as Member[]
+  }),
+  methods: {
+    loadMembers: function() {
+      getAllMembers(this.organization).then(members => {
+        this.members = members;
+      });
+    },
+    onChange(event) {
+     this.organization = event.target.value;
+  },
+  }
+});
+</script>
+
+<style module>
+.table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+.table tbody tr:nth-of-type(odd) {
+  background-color: rgba(0, 0, 0, 0.05);
+}
+</style>
